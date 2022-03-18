d031803095a125549238377724516849daec1cc2
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2022-03-18 17:30:06 +0800

    Little clean up

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ still messy...
 
 1. `python3 -m venv venv`
 1. `source venv/bin/activate`
+1. `pip install -r requirements.txt`
 1. Add `.twitter_keys.ymal`
 1. `python scripts/search_tweets.py --credential-file .twitter_keys.yaml | python scripts/post_tweets.py`
--- scripts/post_tweets.py
+++ scripts/post_tweets.py
@@ -1,38 +1,63 @@
+#!/usr/bin/env python3
+"""
+1. Take JSON string as input
+2. Re-structure the data
+3. POST to Discord webhook
+"""
+
+__author__ = "@hwupu"
+__version__ = "0.0.1"
+__license__ = "MIT"
+
 import sys
 import json
 import requests
 
-source = sys.stdin.readline()
-source = json.loads(source)
+def parse():
+    """ POST to Discord webhook """
+    source = sys.stdin.readline()
+    source = json.loads(source)
+    
+    tweets = source['data']
+    users = {user['id']: user for user in source['includes']['users']}
+    media = {medium['media_key']: medium for medium in source['includes']['media']}
+    referrals = {tweet['id']: tweet for tweet in source['includes']['tweets']}
+    
+    for tweet in tweets:
+        t = tweet
+        if 'referenced_tweets' in tweet:
+            key = tweet['referenced_tweets'][0]['id']
+            t = referrals[key]
+    
+        text = t['text']
+        attachments = [media[m] for m in t['attachments']['media_keys']]
+        user = users[t['author_id']]
+        print(text, user, attachments)
 
-tweets = source['data']
-users = {user['id']: user for user in source['includes']['users']}
-media = {medium['media_key']: medium for medium in source['includes']['media']}
-referrals = {tweet['id']: tweet for tweet in source['includes']['tweets']}
 
-for tweet in tweets:
-    t = tweet
-    if 'referenced_tweets' in tweet:
-        key = tweet['referenced_tweets'][0]['id']
-        t = referrals[key]
+def post():
+    """ POST to Discord webhook """
+    url = ''
+    
+    for tweet in tweets:
+        data = {
+            "content": tweet['text'],
+        }
+        result = requests.post(url, json=data)
+        if 200 <= result.status_code < 300:
+            print(f"Webhook sent {result.status_code}")
+        else:
+            print(f"Not sent with {result.status_code}, response:\n{result.json()}")
+        print(json.dumps(data, indent=2))
 
-    text = t['text']
-    attachments = [media[m] for m in t['attachments']['media_keys']]
-    user = users[t['author_id']]
-    print(text, user, attachments)
 
-exit()
+def main():
+    """ Main entry point of the app """
+    parse()
+    # post()
 
-url = ''
 
-for tweet in tweets:
-    data = {
-        "content": tweet['text'],
-    }
-    result = requests.post(url, json=data)
-    if 200 <= result.status_code < 300:
-        print(f"Webhook sent {result.status_code}")
-    else:
-        print(f"Not sent with {result.status_code}, response:\n{result.json()}")
-    print(json.dumps(data, indent=2))
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
 
