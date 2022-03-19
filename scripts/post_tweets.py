8d7096162345a50e5b1caf501f0321a836a04166
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2022-03-19 22:30:03 +0800

    Add database to store posted tweet id

--- scripts/post_tweets.py
+++ scripts/post_tweets.py
@@ -12,9 +12,14 @@ __license__ = "MIT"
 import sys
 import json
 import requests
+import sqlite3
+from sqlite3 import Error
+
+# Database connection
+conn = None
 
 def parse():
-    """ POST to Discord webhook """
+    """ Prepare the data to post """
     source = sys.stdin.readline()
     source = json.loads(source)
     
@@ -22,7 +27,7 @@ def parse():
     users = {user['id']: user for user in source['includes']['users']}
     media = {medium['media_key']: medium for medium in source['includes']['media']}
     referrals = {tweet['id']: tweet for tweet in source['includes']['tweets']}
-    
+
     for tweet in tweets:
         t = tweet
         text = t['text']
@@ -30,38 +35,82 @@ def parse():
         if 'referenced_tweets' in tweet:
             key = tweet['referenced_tweets'][0]['id']
             t = referrals[key]
-    
-        attachments = [media[m] for m in t['attachments']['media_keys']]
-        user = users[t['author_id']]
 
-        print('username', '{} (@{})'.format(user['name'], user['username']))
-        print('avatar_url', user['profile_image_url'])
-        print('content', text)
-        for a in attachments:
-            print('embeds.image.url', a['url'])
-        print('')
+        if hasTweetBeenPosted(t['id']):
+            print('Tweet has been posted before: {}'.format(t['id']))
+            continue
+        else:
+            attachments = [media[m] for m in t['attachments']['media_keys']]
+            user = users[t['author_id']]
+            
+            print('')
+            print('Posting tweet: {}'.format(t['id']))
+            print('username', '{} (@{})'.format(user['name'], user['username']))
+            print('avatar_url', user['profile_image_url'])
+            print('content', text)
+            for a in attachments:
+                print('embeds.image.url', a['url'])
+
+            payload = {
+                'username': '{} (@{})'.format(user['name'], user['username']),
+                'avatar_url': user['profile_image_url'],
+                'content': text,
+                'embeds': [ {'image': {'url': a['url']}} for a in attachments],
+            } 
+            post(payload)
+            storeTweetToDatabse(t['id'])
 
 
-def post():
+def post(payload):
     """ POST to Discord webhook """
     url = ''
     
-    for tweet in tweets:
-        data = {
-            "content": tweet['text'],
-        }
-        result = requests.post(url, json=data)
-        if 200 <= result.status_code < 300:
-            print(f"Webhook sent {result.status_code}")
-        else:
-            print(f"Not sent with {result.status_code}, response:\n{result.json()}")
-        print(json.dumps(data, indent=2))
+    result = requests.post(url, json=payload)
+    if 200 <= result.status_code < 300:
+        print(f"Webhook sent {result.status_code}")
+    else:
+        print(f"Not sent with {result.status_code}, response:\n{result.json()}")
+
+
+def hasTweetBeenPosted(tweet_id: int):
+    """ Check if tweet_id exist in database, return True if exist """
+    cur = conn.cursor()
+    cur.execute("SELECT id FROM posted_tweets WHERE id = ?", (tweet_id,))
+    data=cur.fetchall()
+    return len(data) != 0
+
+
+def storeTweetToDatabse(tweet_id: int):
+    """ Save tweet_id into database, except record doesn't exist """
+    cur = conn.cursor()
+    cur.execute("INSERT INTO  posted_tweets (id) VALUES (?)", (tweet_id,))
+    conn.commit()
+
+
+def connect_database():
+    """ Create connection to database or initial one if empty """
+    try:
+        global conn
+        conn = sqlite3.connect('db.sqlite3')
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS posted_tweets (id INT)")
+        conn.commit()
+    except Error as e:
+        print(e)
+        raise e
+
+
+def close_databse():
+    """ Close connection to database """
+    if conn:
+        conn.close()
 
 
 def main():
     """ Main entry point of the app """
+    connect_database()
     parse()
-    # post()
+    close_databse()
 
 
 if __name__ == "__main__":
